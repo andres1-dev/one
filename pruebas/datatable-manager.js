265bb9229bf23280d4f6771bf7fdf91cb95115b2
Andrés Mendoza <coordinadorlogisticocdi@gmail.com>
2025-07-24 15:14:57 -0500

    Create datatable-manager.js

---
+++ pruebas/datatable-manager.js
@@ -0,0 +1,57 @@
+// datatable-manager.js
+let dataTable;
+
+export const initializeDataTable = (data) => {
+    // Destruir la tabla existente si hay una
+    if (dataTable) {
+        dataTable.destroy();
+    }
+    
+    dataTable = $('#data-table').DataTable({
+        responsive: true,
+        data: data,
+        columns: [
+            { title: "Documento", data: "DOCUMENTO" },
+            { title: "Fecha", data: "FECHA" },
+            { title: "Taller", data: "TALLER" },
+            { title: "Línea", data: "LINEA" },
+            { title: "Auditor", data: "AUDITOR" },
+            { title: "Escáner", data: "ESCANER" },
+            { title: "Lote", data: "LOTE" },
+            { title: "Ref. Prov.", data: "REFPROV" },
+            { title: "Descripción", data: "DESCRIPCIÓN" },
+            { title: "Cantidad", data: "CANTIDAD" },
+            { title: "Referencia", data: "REFERENCIA" },
+            { title: "Tipo", data: "TIPO" },
+            { title: "PVP", data: "PVP" },
+            { title: "Prenda", data: "PRENDA" },
+            { title: "Género", data: "GENERO" },
+            { title: "Gestor", data: "GESTOR" },
+            { title: "Proveedor", data: "PROVEEDOR" },
+            { title: "Clase", data: "CLASE" }
+        ],
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'print',
+            /*{
+                text: 'Actualizar',
+                action: function(e, dt, node, config) {
+                    updateData();
+                }
+            }*/
+        ],
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+        },
+        pageLength: 25,
+        order: [[1, 'desc']]
+    });
+};
+
+export const updateDataTable = (newData) => {
+    if (dataTable) {
+        dataTable.clear().rows.add(newData).draw();
+    } else {
+        initializeDataTable(newData);
+    }
+};
