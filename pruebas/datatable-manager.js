e67ce5f07d8caa4197862986d31b2e1677dcea1c
Andrés Mendoza <coordinadorlogisticocdi@gmail.com>
2025-07-25 09:01:36 -0500

    Update datatable-manager.js

--- pruebas/datatable-manager.js
+++ pruebas/datatable-manager.js
@@ -59,36 +59,32 @@ export const initializeDataTable = (data) => {
         ],
         dom: '<"top"<"row"<"col-md-6"l><"col-md-6"f>>><"row"<"col-md-12"tr>><"bottom"<"row"<"col-md-5"i><"col-md-7"p>>>',
         buttons: [
-{
+            {
                 extend: 'excel',
                 text: '<i class="fas fa-file-excel me-1"></i> Excel',
                 className: 'btn btn-success btn-sm',
                 filename: 'Reporte_Ingresos',
-                //title: 'Reporte de Ingresos',
+                title: 'Reporte de Ingresos',
                 exportOptions: {
-                    //columns: ':visible',
-                    columns: ':all', // Exportar todas las columnas, no solo las visibles
+                    columns: ':all',
                     format: {
                         body: function(data, row, column, node) {
                             // Mantener formato de fechas en Excel
                             if (column === 1 && data.match(/^\d{2}\/\d{2}\/\d{4}$/)) {
                                 const [day, month, year] = data.split('/');
-                                return ${year}-${month}-${day};
+                                return `${year}-${month}-${day}`;
                             }
                             return data;
                         }
                     }
                 }
-            }, 
-
-
+            },
             {
                 extend: 'print',
                 text: '<i class="fas fa-print me-1"></i> Imprimir',
                 className: 'btn btn-primary btn-sm',
                 title: 'Reporte de Ingresos',
                 exportOptions: {
-                    //columns: ':all', // Exportar todas las columnas, no solo las visibles             
                     columns: ':visible'
                 },
                 customize: function(win) {
@@ -100,7 +96,7 @@ export const initializeDataTable = (data) => {
         language: {
             url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
         },
-        pageLength: 5,
+        pageLength: 25,
         order: [[1, 'desc']],
         responsive: {
             details: {
