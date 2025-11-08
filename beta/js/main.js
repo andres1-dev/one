510ca7f2e4d8eadc63dfc398bbcaf06215c2d413
Andrés Mendoza <coordinadorlogisticocdi@gmail.com>
2025-11-08 00:46:01 -0500

    Update main.js

--- beta/js/main.js
+++ beta/js/main.js
@@ -4,6 +4,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Inicializar la cola de carga
   initializeUploadQueue();
+
+  // Inicializar controlador de teclado y QR
+  initializeInputController();
   
   // Cargar datos desde el servidor
   loadDataFromServer();
